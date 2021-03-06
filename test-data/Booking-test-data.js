92223d6a7610f19309f6b39cb1f5f2630626babb
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-05 18:39:12 -0700

    Create test data files for class tests

---
+++ test-data/Booking-test-data.js
@@ -0,0 +1,38 @@
+const bookingData = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 1,
+    "date": "2020/04/22",
+    "roomNumber": 1,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 1,
+    "date": "2020/01/24",
+    "roomNumber": 4,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 2,
+    "date": "2020/01/10",
+    "roomNumber": 5,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 2,
+    "date": "2020/02/16",
+    "roomNumber": 6,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 3,
+    "date": "2020/02/05",
+    "roomNumber": 3,
+    "roomServiceCharges": []
+  }]
+
+  export default bookingData; 
