f32af0caa79ca7d6963304d0f83f38e5cb2196cd
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-08 21:42:57 -0700

    Add Manager class and methods, pass tests

--- src/Manager.js
+++ src/Manager.js
@@ -1,8 +1,41 @@
-// class Manager extends User {
-//   constructor(){
-//     super()
-//     this.bookings = [{date: 4/5/2020, roomType: suite, cost: $345.21}]; 
-//   }
+import User from "./User";
+
+class Manager  {
+  constructor(userData){
+    this.selectedUser = '';
+    this.users = userData; 
+  }
+
+  searchUsers(name) {
+    const selectedUser = this.users.find(user => {
+      const firstandLastName = user.name.split(' ');
+      return firstandLastName.includes(name);
+    });
+    if(!selectedUser) {
+      this.selectedUser = '';
+  } else {
+      const user = new User(selectedUser);  
+      this.selectedUser = user; 
+  }
+  }
+  deleteBooking(bookingId) {
+    const bookingIndex = this.selectedUser.bookings.findIndex(booking => {
+      return booking.id === bookingId;
+    });
+    this.selectedUser.bookings.splice(bookingIndex, 1); 
+  }
+  
+  createNewBooking(roomNum, date) {
+    return {
+      userID: this.selectedUser.id,
+      date: date,
+      roomNumber: roomNum
+    }
+  }
+}
+
+
+export default Manager; 
 
 //Have a manager login button that is clicked and will validate if the manager can sign in or not
 
@@ -10,6 +43,12 @@
    //manager can search a user by name and a current User is intitated
    // manager can use thier name, and bookings array
    //
+
+   //METHODS 
+
+   //Search users
+   //method that will search the user's array and find the user match
+   // should return user 
 
     //deleteBooking {
       // method will delete booking from the user's array
